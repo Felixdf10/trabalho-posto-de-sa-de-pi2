262ccbf7be13ce38faac97d3667fb0c1aa3270d5
Felixdf10 <felipe.flor@academico.ifpb.edu.br>
2025-08-27 13:20:26 +0000

    felipe

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import apiRoutes from './routes.js';
+
+// Define __dirname manualmente (porque estamos usando ES Modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
+// Rota para a raiz '/', serve o arquivo index.html
 app.get('/', (req, res) => {
-  res.send('API do Posto de Saúde em funcionamento!');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Usa todas as rotas definidas no arquivo apiRoutes.js
